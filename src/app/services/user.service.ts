77536cf11570a5d8bc3a1b6a9d95ea1ffe778e0b
dero-hub <munenederrick862@gmail.com>
2022-06-23 16:34:42 +0300

    Added a users service and integrated rest apis using axios library

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,28 @@
+// import { Injectable} from '@angular/core';
+// import axios from 'axios';
+
+
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class UserService {
+
+//   constructor() { }
+
+//   async signup(data: any) {
+
+//     axios.post('http://localhost:4000/api/v1/users/create', data)
+//       .then(function (response) {
+//         // handle success
+//         console.log(response.data);
+//         return response.data;
+//       })
+//       .catch(function (error) {
+//         // handle error
+//         console.log(error);
+//         return error;
+//       })
+
+//   }
+// }
