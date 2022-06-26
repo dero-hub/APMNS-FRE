e4231d4ce6f891ff591b709bdb7aa47ef0a7469b
dero-hub <munenederrick862@gmail.com>
2022-06-26 17:43:41 +0300

    Added signup logic to the home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SignupComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import {Router} from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private zone: NgZone, private http: UserService) { }
+
+  register: User = {
+    firstname:'',
+    lastname: '',
+    email: '',
+    phone:'',
+    password: ''
+    
+  }
+  submitted = false;
 
   ngOnInit(): void {
   }
 
-}
+  saveUser() {
+   
+    const userDetails = {
+      firstname: this.register.firstname,
+      lastname: this.register.lastname,
+      email: this.register.email,
+      phone: this.register.phone.toString(),
+      password: this.register.password
+      }
+      console.log(userDetails);
+      this.http.signup(userDetails);
+      this.submitted = true
+      
+    };
+
+    newUser(): void {
+      this.submitted = false;
+      this.register = {
+        firstname: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        password: ''
+        
+      };
+    }
+  }
