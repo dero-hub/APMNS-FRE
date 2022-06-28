48ab8fa2478f874b24280e0e814076d11e6701aa
dero-hub <munenederrick862@gmail.com>
2022-06-28 17:06:03 +0300

    Added logic and views to handle login functionality

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: any = {
+    phone: '',
+    password: ''
+  }
+  submitted = false;
+
+
+  constructor(private router: Router, private zone: NgZone, private http: UserService) { }
 
   ngOnInit(): void {
   }
+
+  saveUser() {
+    const userDetails = {
+      phone: this.user.phone,
+      password: this.user.password
+    };
+    // this.submitted = true;
+    return this.http.signin(userDetails)
+  }
+
+  // async register() {
+//     await this.zone.run(async () => {
+//       this.router.navigate(['/user/signup']).then(r => console.log(r));
+//     });
+//   }
+
+//   async resetPwd() {
+//     await this.zone.run(async () => {
+//       this.router.navigate(['/user/resetpwd']).then(r => console.log(r));
+//     });
+// }
+
+// async home() {
+//   await this.zone.run(async () => {
+//     this.router.navigate(['']).then(r => console.log(r));
+//   });
+// }
+
+// async dashboard() {
+//   await this.zone.run(async () => {
+//     this.router.navigate(['/user/dashboard']).then(r => console.log(r));
+//   });
+// }
 
 }
