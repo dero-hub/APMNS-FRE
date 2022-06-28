b13820ffe4904fafc2708cb16f125ebdf24257e1
dero-hub <munenederrick862@gmail.com>
2022-06-28 16:51:34 +0300

    Added a login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
