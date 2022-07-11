a217d935a395a1c462f030fbf4c05b1be1f7be6d
dero-hub <munenederrick862@gmail.com>
2022-07-11 21:53:53 +0300

    Changed the homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
